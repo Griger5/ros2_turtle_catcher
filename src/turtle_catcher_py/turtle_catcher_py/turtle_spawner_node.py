adcdb3d172cfaa88dacc944458acae8d704e1c7b
Griger5 <griger04@gmail.com>
2024-11-11 19:28:37 +0100

    turtle_spawner_node now publishes all alive turtle names to /turtle_names

--- src/turtle_catcher_py/turtle_catcher_py/turtle_spawner_node.py
+++ src/turtle_catcher_py/turtle_catcher_py/turtle_spawner_node.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn
+from turtle_catcher_interfaces.msg import TurtleArray
 
 import random
 
@@ -9,7 +10,11 @@ class TurtleSpawnerNode(Node):
     def __init__(self):
         super().__init__("turtle_spawner_node")
 
+        self.turtle_names = []
+
         self.spawner_timer = self.create_timer(2.0, self.call_spawn_turtle_service)
+
+        self.turtle_names_publisher_ = self.create_publisher(TurtleArray, "/turtle_names", 10)
 
     def call_spawn_turtle_service(self):
         client = self.create_client(Spawn, "/spawn")
@@ -25,8 +30,19 @@ class TurtleSpawnerNode(Node):
     def turtle_spawn_callback(self, future):
         try:
             response = future.result()
+
+            self.turtle_names.append(response.name)
+            self.publish_turtle_names()
+
         except Exception as e:
             self.get_logger().error(f"Service call failed: {e}")
+
+    def publish_turtle_names(self):
+        msg = TurtleArray()
+
+        msg.turtle_names = self.turtle_names
+
+        self.turtle_names_publisher_.publish(msg)
 
 
 def main(args=None):
