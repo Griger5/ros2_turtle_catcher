a0d0f84f2504251c2663149beb379f4d1c7e7cff
Griger5 <griger04@gmail.com>
2024-11-11 19:56:29 +0100

    Modified turtle_spawner_node to also include turtles' x and y coordinates

--- src/turtle_catcher_py/turtle_catcher_py/turtle_spawner_node.py
+++ src/turtle_catcher_py/turtle_catcher_py/turtle_spawner_node.py
@@ -2,47 +2,57 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn
-from turtle_catcher_interfaces.msg import TurtleArray
+from turtle_catcher_interfaces.msg import Turtle, TurtleArray
 
 import random
+from functools import partial
 
 class TurtleSpawnerNode(Node):
     def __init__(self):
         super().__init__("turtle_spawner_node")
 
-        self.turtle_names = []
+        self.turtles = []
 
         self.spawner_timer = self.create_timer(2.0, self.call_spawn_turtle_service)
 
-        self.turtle_names_publisher_ = self.create_publisher(TurtleArray, "/turtle_names", 10)
+        self.turtles_publisher_ = self.create_publisher(TurtleArray, "/turtles", 10)
 
     def call_spawn_turtle_service(self):
         client = self.create_client(Spawn, "/spawn")
 
         while not client.wait_for_service(1.0):
             self.get_logger().warn("Waiting for service...")
+
+        turtle_x = random.uniform(0.1, 10.9)
+        turtle_y = random.uniform(0.1, 10.9)
         
-        request = Spawn.Request(x = random.uniform(0.1, 10.9), y = random.uniform(0.1, 10.9), theta = random.uniform(-3.14, 3.14))
+        request = Spawn.Request(x = turtle_x, y = turtle_y, theta = random.uniform(-3.14, 3.14))
 
         future = client.call_async(request)
-        future.add_done_callback(self.turtle_spawn_callback)
+        future.add_done_callback(partial(self.turtle_spawn_callback, x = turtle_x, y = turtle_y))
         
-    def turtle_spawn_callback(self, future):
+    def turtle_spawn_callback(self, future, x, y):
         try:
             response = future.result()
 
-            self.turtle_names.append(response.name)
-            self.publish_turtle_names()
+            new_turtle = Turtle()
+
+            new_turtle.name = response.name
+            new_turtle.x = x
+            new_turtle.y = y
+
+            self.turtles.append(new_turtle)
+            self.publish_turtles()
 
         except Exception as e:
             self.get_logger().error(f"Service call failed: {e}")
 
-    def publish_turtle_names(self):
+    def publish_turtles(self):
         msg = TurtleArray()
 
-        msg.turtle_names = self.turtle_names
+        msg.turtles = self.turtles
 
-        self.turtle_names_publisher_.publish(msg)
+        self.turtles_publisher_.publish(msg)
 
 
 def main(args=None):
